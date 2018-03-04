656dd542b2ca89d0920c520dab9a3222f8eb6804
saigon3748 <saigon3748@gmail.com>
2018-03-04 19:56:20 +0800

    updated

--- src/server/domain/order/service.js
+++ src/server/domain/order/service.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
 import moment from 'moment';
+import Promise from 'bluebird';
 import promiseRetry from 'promise-retry';
 import schema from './schema';
 import BaseService from '../base-service';
+import KitchenService from '../kitchen/service';
 import pipeline from '../../libs/pipeline';
 
 export default class Service extends BaseService {
@@ -29,6 +31,8 @@ export default class Service extends BaseService {
     if (data.items.length === 0) throw new Error('Missing items');
 
     let doCreate = () => {
+      let order;
+
       let doGenerateCode = () => {
         data.code = this._generateCode(this._ctx.user.tenant.code);
         data.ref = data.code.slice(-4);
@@ -56,13 +60,41 @@ export default class Service extends BaseService {
       }
 
       let doSave = () => {
-        return super.create(data);   
+        return super.create(data)
+          .then(result => {
+            order = result;
+          })   
+      }
+
+      let doCreateKitchen = () => {
+        let doCreateKitchenItem = (item) => {
+          return new KitchenService(this._ctx)
+            .create({
+              tenant: order.tenant,
+              orderId: order._id,
+              orderCode: order.code,
+              orderRef: order.ref,
+              itemId: item._id,
+              name: item.name,
+              quantity: item.quantity,
+              category: item.category,
+              note: item.note
+            })
+        }
+
+        return Promise.each(order.items, doCreateKitchenItem);
+      }
+
+      let doReturn = () => {
+        return order;
       }
 
       return pipeline([
         doGenerateCode,
         doCalculate,
-        doSave
+        doSave,
+        doCreateKitchen,
+        doReturn
       ])
     }
 
